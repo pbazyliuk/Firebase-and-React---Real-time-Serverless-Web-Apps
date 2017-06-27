a81d976dd139fc4bed5b25c563f8582a60e7c014
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-06-27 17:45:30 +0300

    connecting React to Firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    database.ref('/').on('value', () => {
+      console.log('THE DATA CHANGED');
+    });
+  }
+
   render() {
     return (
       <div className="App">
